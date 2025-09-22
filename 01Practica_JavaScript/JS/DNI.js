49d892db1fd2157c98bef552b84915877d0e558b
DMCHKN3 <ejimenezv2304@alumno.ipn.mx>
2025-09-21 22:48:15 -0600

    se cambian los nombres de los ids para que no haya conflicto con las variables

--- 01Practica_JavaScript/JS/DNI.js
+++ 01Practica_JavaScript/JS/DNI.js
@@ -1,4 +1,4 @@
-const verificar = document.getElementById('verificar');
+const verificar = document.getElementById('btn-verificar');
 
 var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B',
              'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
@@ -8,8 +8,8 @@ verificar.addEventListener('click', () => {
 });
 
 function validacionDNI() {
-    const numDNI = document.getElementById('numDNI').value;
-    const letraDNI = document.getElementById('letraDNI').value.toUpperCase();
+    const numDNI = document.getElementById('txt-numDNI').value;
+    const letraDNI = document.getElementById('txt-letraDNI').value.toUpperCase();
 
     if (numDNI.trim() === '' || letraDNI.trim() === '' || !Number.isInteger(Number(numDNI))) {
         return 'Por favor, ingrese un número de DNI válido.';
